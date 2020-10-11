39191b2eeb7d84b757f28b1535eba72ee2cf1a71
Poly-almeida <70905626+Poly-almeida@users.noreply.github.com>
2020-10-11 17:39:37 -0300

    Add files via upload

---
+++ src/model/FieldInfo.cpp
@@ -0,0 +1,46 @@
+
+#include "FieldInfo.h"
+#include "AttributesInfo.h"
+#include "../constants/AcessFlagsFieldConst.h"
+#include "../constants/AcessFlagsClassConst.h"
+
+
+void showFields(FieldInfo *fields, unit16_t fields_count){
+	int i;
+
+	for(i=0; i<fields_count; i++){
+	
+	switch(fields->access_flags){
+		case(PUBLIC):
+			printf("\tFields - Access Flags: Public\n");
+			break;
+		case(PRIVATE):
+			printf("\tFields - Access Flags: Private\n");
+			break;
+		case(PROTECTED):
+			printf("\tFields - Access Flags: Protected\n");
+			break;
+		case(STATIC):
+			printf("\tFields - Access Flags: Static\n");
+			break;
+		case(FINAL):
+			printf("\tFields - Access Flags: Final\n");
+			break;
+		case(VOLATILE):
+			printf("\tFields - Access Flags: Volatile\n");
+			break;
+		case(TRANSCIENT):
+			printf("\tFields - Access Flags: Transcient\n");
+			break;
+		case(ENUM):
+			printf("\tFields - Access Flags: Enum\n");
+			break;
+	}
+	printf("\tFields - Name Index: %02x\n", fields->name_index);
+	printf("\tFields - Descriptor Index: %02x\n", fields->descriptor_index);
+	printf("\tFields - Attributes Count: %02x\n", fields->attributes_count);
+
+	
+
+	}
+}
