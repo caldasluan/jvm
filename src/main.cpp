6170dcb36f47c64f345538a39b67a476dd194100
viniap <viniciusgalego8@live.com>
2020-10-13 21:48:39 -0300

    adição dos parâmetros via linha de comando

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,30 @@
 
 int main(int argc, char *argv[])
 {
-    char file[] = "input/MainAct.class";
-
-    ClassFile* classFile = ReadModule::read_file(file);
-    DisplayModule::show(*classFile);
-
-    delete classFile;
+    if(argc > 1 && argc <= 3) {
+        char *file = argv[1];
+        ClassFile* classFile = ReadModule::read_file(file);
+        if(argc == 3) {
+            if(argv[2][1] == 'i') {
+                DisplayModule::show(*classFile);
+            }
+            else {
+                printf("Opção inválida\n");
+                delete classFile;
+                return 0;
+            }
+        }
+        else {
+            printf("Execução em desenvolvimento\n");
+            delete classFile;
+            return 0;
+        }
+        delete classFile;
+    }
+    else {
+        printf("\nExecução: ./jvm arquivo opção\n");
+        printf("\nOpções:\n");
+        printf("\t-i: leitor/exibidor de bytecode\n\n");
+        return 0;
+    }
 }
