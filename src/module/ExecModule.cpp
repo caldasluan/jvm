bdda3f9f2738eb0f13fbfa455c34fbb88594c58b
Luan Caldas <luanpignat@gmail.com>
2020-11-07 20:50:46 -0300

    Ajustar vazamento de memória

--- src/module/ExecModule.cpp
+++ src/module/ExecModule.cpp
@@ -27,7 +27,6 @@ void ExecModule::exec_jvm(Runtime &runtime) {
             if(frame->ret_words == 1)
             {
                 uint32_t ret = frame->operand_stack.top();
-                delete runtime.stack_frames.top();
                 runtime.stack_frames.pop();
                 runtime.stack_frames.top()->operand_stack.push(ret);
             }
@@ -36,7 +35,6 @@ void ExecModule::exec_jvm(Runtime &runtime) {
                 uint32_t ret1 = frame->operand_stack.top();
                 frame->operand_stack.pop();
                 uint32_t ret2 = frame->operand_stack.top();
-                delete runtime.stack_frames.top();
                 runtime.stack_frames.pop();
                 runtime.stack_frames.top()->operand_stack.push(ret2);
                 runtime.stack_frames.top()->operand_stack.push(ret1);
