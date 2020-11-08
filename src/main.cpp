09b44dad09c9e810e476a572af519cde55712882
viniap <viniciusgalego8@live.com>
2020-11-08 13:16:44 -0300

    desfazendo alteração na main

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,35 @@
 #include "module/ReadModule.h"
 #include "module/DisplayModule.h"
 #include "module/ExecModule.h"
-#include <cstring>
-
-using namespace std;
-
-void help ()
-{
-    printf("\nExecucao: ./jvm [opcao] arquivo\n");
-    printf("\nOpcoes:\n");
-    printf("\t-i: Leitor/exibidor de bytecode\n\n");
-}
 
 // JVM tem que passar argumentos pela linha de comando pro programa executado.
 int main(int argc, char *argv[])
 {
-    if (argc > 1 && argc < 4)
+    if (argc > 1)
     {
-        if (argc == 2)
+        if (argc == 3 && argv[1][0] == '-' && argv[1][1] == 'i')
         {
-            char *fileName = argv[1];
-            ExecModule::initialize_jvm(fileName, argc, argv);
+            char *fileName = argv[2];
+            ClassFile *classFile = ReadModule::read_file(fileName);
+            if (classFile == nullptr)
+                return 0;
+
+            DisplayModule::show(*classFile);
+            delete classFile;
         }
         else
         {
-            if (argv[1][0] == '-' && argv[1][1] == 'i' && strlen(argv[1]) == 2)
-            {
-                char *fileName = argv[2];
-                ClassFile *classFile = ReadModule::read_file(fileName);
-                if (classFile == nullptr)
-                    return 0;
 
-                DisplayModule::show(*classFile);
-                delete classFile;
-            }
-            else
-            {
-                help();
-            }
+            char *fileName = argv[1];
+            ExecModule::initialize_jvm(fileName, argc, argv);
         }
     }
     else
     {
-        help();
+        printf("\nExecucao: ./jvm arquivo opcao\n");
+        printf("\nOpcoes:\n");
+        printf("\t-i: leitor/exibidor de bytecode\n\n");
     }
     return 0;
 }
+
